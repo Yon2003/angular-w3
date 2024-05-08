2c410f6065ced7f07af4b5349cdead12574b08e3
Yon2003 <127757005+Yon2003@users.noreply.github.com>
2024-05-08 14:42:42 +0300

    note

--- note/src/app/app.component.ts
+++ note/src/app/app.component.ts
@@ -1,13 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'note';
+
+  public index = 0;
+
+  public tempTitle = '';
+  public tempDescription = '';
+
+
+  public NotCollection: { title: string, description: string }[] = [];
+
+
+
+  public InputTitle(input) {
+    this.tempTitle = input.target;
+  }
+  public InputDescription(input) {
+    this.tempDescription = input.target;
+  }
+
+  public SaveNote() {
+    this.index++;
+
+    this.NotCollection[this.index].title = this.tempTitle;
+    this.NotCollection[this.index].description = this.tempDescription;
+    this.resetTempData();
+  }
+
+  public processVideoRecord(videoElement, index) {
+    this.index = index;
+  }
+
+  private resetTempData() {
+    this.tempTitle = '';
+    this.tempDescription = '';
+  }
 }
