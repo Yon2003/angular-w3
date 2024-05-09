20369d5fa4fd3176e18370fdb9fd1832304acb77
Yon2003 <127757005+Yon2003@users.noreply.github.com>
2024-05-09 11:33:31 +0300

    note

--- note/src/app/app.component.ts
+++ note/src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'note';
 
   public index = 0;
 
   public tempTitle = '';
   public tempDescription = '';
+
+  public modelTitle = '';
+  public modelDescription = '';
 
 
   public NotCollection: { title: string, description: string }[] = [];
@@ -23,24 +26,34 @@ export class AppComponent {
 
 
   public InputTitle(input) {
-    this.tempTitle = input.target;
+    this.tempTitle = input.target.value;
   }
   public InputDescription(input) {
-    this.tempDescription = input.target;
+    this.tempDescription = input.target.value;
   }
+
+
 
   public SaveNote() {
 
-    this.NotCollection[this.index].title = this.tempTitle;
-    this.NotCollection[this.index].description = this.tempDescription;
+    if (this.NotCollection.length > this.index) {
+      this.NotCollection[this.index].title = this.tempTitle;
+      this.NotCollection[this.index].description = this.tempDescription;
+    } else {
+      this.NotCollection.push({ title: this.tempTitle, description: this.tempDescription });
+    }
     this.resetTempData();
+    console.log("NotCollection:", this.NotCollection);
   }
 
-  public processVideoRecord(videoElement, index) {
+  public EditBut(videoElement, index) {
     this.index = index;
   }
 
+
+
   private resetTempData() {
+    this.index++;
     this.tempTitle = '';
     this.tempDescription = '';
   }
